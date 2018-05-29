d7de2a3233a494f9f076c3483cf205881e53c5d6
Pradeep Kumar Tippa <pktippa@gmail.com>
2018-05-29 15:07:54 +0530

    Updated Questions in JS

---
+++ computer_science/programming_languages/js/Questions.md
@@ -0,0 +1,6 @@
+# Questions
+
+1. When to use `function.apply`
+A: Check [function_apply](quest/function_apply.js)
+
+2. What is difference between `arr.splice` and `arr.slice` methods in array.
---
+++ computer_science/programming_languages/js/quest/function_apply.js
@@ -0,0 +1,25 @@
+function fullname(){
+    return this.firstName + ' ' + this.lastName;
+}
+
+const person = {
+    firstName: 'Pradeep',
+    lastName: 'Kumar'
+}
+// Prints 'Pradeep Kumar'
+console.log(fullname.apply(person));
+
+function fullname_withArgs(city, country) {
+    return this.firstName + ' ' + this.lastName + ' is from ' + city + ', ' + country;
+}
+
+const person1 = {
+    firstName: 'Tippa',
+    lastName: 'Kumar'
+};
+
+
+// the .apply() method takes arguments as array
+console.log(fullname_withArgs.apply(person1, ['Hyderabad', 'India']));
+// Prints 'Tippa Kumar is from Hyderabad, India'
+
